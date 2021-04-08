fbf16062aa16253ddc63f9f118166f54b384f603
Indrajith V <indrajithvgp@yahoo.com>
2021-04-08 21:21:22 +0530

    checkout functionality and order data modeling done

--- front-end/src/screens/ShippingScreen.js
+++ front-end/src/screens/ShippingScreen.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {Form, Button} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
-import Message from '../components/Message'
+
 import Loader from '../components/Loader'
 import FormContainer from '../components/FormContainer'
 import CheckoutSteps from '../components/CheckoutSteps'
--- front-end/src/store.js
+++ front-end/src/store.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import {productReducer, productDetailsReducer} from './reducers/productReducer'
 import {cartReducer} from './reducers/cartReducers'
+import {orderCreateReducer} from './reducers/orderReducer'
 import {userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer} from './reducers/userReducers'
 
 const reducer = combineReducers({
@@ -13,6 +14,7 @@ const reducer = combineReducers({
     userRegister:userRegisterReducer,
     userDetails:userDetailsReducer,
     userUpdatedProdile:userUpdateProfileReducer,
+    orderCreate:orderCreateReducer
 
 
 })
