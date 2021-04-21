cbcaa6801d8ca80717a511c69e5d2ede38af4561
Indrajith V <indrajithvgp@yahoo.com>
2021-04-21 22:09:21 +0530

    navbar search functionality added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -27,6 +27,7 @@ const App=()=> {
       <main className="py-3">
         <Container>
           <Route path='/' exact component={HomeScreen}/>
+          <Route path='/search/:keyword' component={HomeScreen}/>
           <Route path='/product/:id' component={ProductScreen}/>
           <Route path='/cart/:id?' component={CartScreen}/>
           <Route path='/login' component={LoginScreen}/>
--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -1,37 +1,62 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
-import {useDispatch, useSelector} from 'react-redux'
 import Product from '../components/Product'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import {listProducts} from '../actions/productActions'
+import Paginate from '../components/Paginate'
+import ProductCarousel from '../components/ProductCarousel'
+import Meta from '../components/Meta'
+import { listProducts } from '../actions/productActions'
 
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword
 
+  const pageNumber = match.params.pageNumber || 1
 
-function HomeScreen() {
+  const dispatch = useDispatch()
 
-    const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products, page, pages } = productList
 
-    const productList = useSelector(state => state.productList)
-    const {loading, error, products} = productList
-    useEffect(()=>{
-        dispatch(listProducts())
-    },[dispatch])
-    
-    return (
+  useEffect(() => {
+    dispatch(listProducts(keyword, pageNumber))
+  }, [dispatch, keyword, pageNumber])
+
+  return (
+    <>
+      <Meta />
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go Back
+        </Link>
+      )}
+      <h1>Latest Products</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
         <>
-            <h1>Latest Products</h1>
-            { loading ? <Loader/> : error ? <Message variant="danger">{error}</Message> : 
-            <Row>
-            {products.map(product => (
-                <Col key={product._id} sm={12} md={6} lg={4}>
-                    <Product product={product}/>
-                </Col>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
             ))}
-            </Row>}
-            
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ''}
+          />
         </>
-    )
+      )}
+    </>
+  )
 }
 
 export default HomeScreen
